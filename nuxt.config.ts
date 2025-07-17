a6d96f3fce7141668d671c6a1e39f9dc3bb7891e
EllipseHEP <cyrille.mathieu-ton@hepl.ch>
2025-07-17 10:07:27 +0200

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  ssr: false,
+  css: ["@/assets/css/main.css"],
+  compatibilityDate: "2025-05-15",
+  devtools: { enabled: true },
+
+  vite: {
+    plugins: [
+      tailwindcss()
+    ],  
+  },
+
+  modules: ["@pinia/nuxt"],
+})
